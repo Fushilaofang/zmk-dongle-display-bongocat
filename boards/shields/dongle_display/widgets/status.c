56d26f3ee537708fc11137bc9046b4bccee0f5ea
fushilaofang <1770438260@qq.com>
2025-10-10 07:32:45 +0800

    取消wpm旋转

--- boards/shields/dongle_display/widgets/status.c
+++ boards/shields/dongle_display/widgets/status.c
@@ -32,7 +32,7 @@ struct wpm_status_state {
     uint8_t wpm;
 };
 
-static void draw_wpm_canvas(lv_obj_t *canvas_obj, lv_color_t cbuf[], const struct status_state *state) {
+static void draw_wpm_canvas(lv_obj_t *canvas_obj, const struct status_state *state) {
     lv_obj_t *canvas = canvas_obj;
 
     lv_draw_label_dsc_t label_dsc_wpm;
@@ -79,8 +79,6 @@ static void draw_wpm_canvas(lv_obj_t *canvas_obj, lv_color_t cbuf[], const struc
     }
     lv_canvas_draw_line(canvas, points, 10, &line_dsc);
 
-    /* Rotate canvas if needed (rotate_canvas expects child canvas + cbuf) */
-    rotate_canvas(canvas, cbuf);
 }
 
 static void set_wpm_status(struct zmk_widget_status *widget, struct wpm_status_state state) {
@@ -89,7 +87,7 @@ static void set_wpm_status(struct zmk_widget_status *widget, struct wpm_status_s
     }
     widget->state.wpm[9] = state.wpm;
 
-    draw_wpm_canvas(widget->obj, widget->cbuf, &widget->state);
+    draw_wpm_canvas(widget->obj, &widget->state);
 }
 
 static void wpm_status_update_cb(struct wpm_status_state state) {
