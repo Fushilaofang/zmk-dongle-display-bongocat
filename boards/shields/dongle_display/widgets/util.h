38489b4f634972a4fd64c90e5ab82982141a96fd
fushilaofang <1770438260@qq.com>
2025-10-10 11:02:21 +0800

    uint8_t wpm[20]

--- boards/shields/dongle_display/widgets/util.h
+++ boards/shields/dongle_display/widgets/util.h
@@ -18,7 +18,7 @@
     IS_ENABLED(CONFIG_NICE_VIEW_WIDGET_INVERTED) ? lv_color_white() : lv_color_black()
 
 struct status_state {
-    uint8_t wpm[10];
+    uint8_t wpm[20];
 };
 
 void init_label_dsc(lv_draw_label_dsc_t *label_dsc, lv_color_t color, const lv_font_t *font,
