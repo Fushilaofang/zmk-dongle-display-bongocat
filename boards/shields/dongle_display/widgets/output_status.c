700259aae2824e7228fee53e34072488d27c6236
fushilaofang <1770438260@qq.com>
2025-10-11 01:59:26 +0800

    优化配置：将LV_DPI_DEF的默认值从168调整为148

--- boards/shields/dongle_display/widgets/output_status.c
+++ boards/shields/dongle_display/widgets/output_status.c
@@ -179,9 +179,7 @@ ZMK_SUBSCRIPTION(widget_output_status, zmk_usb_conn_state_changed);
 int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_obj_t *parent) {
     widget->obj = lv_obj_create(parent);
 
-    lv_obj_set_size(widget->obj, 50, 30);  // 增加固定尺寸确保足够空间
-    lv_obj_clear_flag(widget->obj, LV_OBJ_FLAG_SCROLLABLE);
-    lv_obj_set_style_clip_corner(widget->obj, false, 0);
+    lv_obj_set_size(widget->obj, LV_SIZE_CONTENT, LV_SIZE_CONTENT);
 
     lv_obj_t *usb = lv_img_create(widget->obj);
     lv_obj_align(usb, LV_ALIGN_TOP_LEFT, 1, 4);
@@ -196,11 +194,9 @@ int zmk_widget_output_status_init(struct zmk_widget_output_status *widget, lv_ob
 
     lv_obj_t *bt_number = lv_img_create(widget->obj);
     lv_obj_align_to(bt_number, bt, LV_ALIGN_OUT_RIGHT_TOP, 2, 7);
-    lv_obj_clear_flag(bt_number, LV_OBJ_FLAG_OVERFLOW_VISIBLE);
 
     lv_obj_t *bt_status = lv_img_create(widget->obj);
     lv_obj_align_to(bt_status, bt, LV_ALIGN_OUT_RIGHT_TOP, 2, 1);
-    lv_obj_clear_flag(bt_status, LV_OBJ_FLAG_OVERFLOW_VISIBLE);
     
     static lv_style_t style_line;
     lv_style_init(&style_line);
