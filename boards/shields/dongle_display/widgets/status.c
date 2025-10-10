a448f07b19d33048c7055a46659470170e601bb2
fushilaofang <1770438260@qq.com>
2025-10-10 10:12:18 +0800

    fix # Please enter the commit message for your changes. Lines starting

--- boards/shields/dongle_display/widgets/status.c
+++ boards/shields/dongle_display/widgets/status.c
@@ -47,16 +47,18 @@ static void draw_wpm_canvas(lv_obj_t *canvas_obj, const struct status_state *sta
     /* Fill background */
     lv_canvas_draw_rect(canvas, 0, 0, CANVAS_SIZE, CANVAS_SIZE, &rect_black_dsc);
 
-    /* Draw WPM box and value (vertically centered) */
+    /* Draw WPM box and value (centered) */
+    const int box_w = 68;
     const int box_h = 42;
-    const int box_top = (CANVAS_SIZE - box_h) / 2; /* center vertically */
-    lv_canvas_draw_rect(canvas, 0, box_top, 68, box_h, &rect_white_dsc);
-    lv_canvas_draw_rect(canvas, 1, box_top + 1, 66, box_h - 2, &rect_black_dsc);
+    const int box_left = (CANVAS_SIZE - box_w) / 2; /* center horizontally */
+    const int box_top = (CANVAS_SIZE - box_h) / 2;  /* center vertically */
+    lv_canvas_draw_rect(canvas, box_left, box_top, box_w, box_h, &rect_white_dsc);
+    lv_canvas_draw_rect(canvas, box_left + 1, box_top + 1, box_w - 2, box_h - 2, &rect_black_dsc);
 
     char wpm_text[6] = {};
     snprintf(wpm_text, sizeof(wpm_text), "%d", state->wpm[9]);
-    /* place WPM value relative to box_top */
-    lv_canvas_draw_text(canvas, 42, box_top + 31, 24, &label_dsc_wpm, wpm_text);
+    /* place WPM value relative to box */
+    lv_canvas_draw_text(canvas, box_left + 42, box_top + 31, 24, &label_dsc_wpm, wpm_text);
 
     int max = 0;
     int min = 256;
@@ -77,7 +79,7 @@ static void draw_wpm_canvas(lv_obj_t *canvas_obj, const struct status_state *sta
 
     lv_point_t points[10];
     for (int i = 0; i < 10; i++) {
-        points[i].x = 2 + i * 7;
+        points[i].x = box_left + 2 + i * 7;
         /* graph area height = box_h - 2 (inner), map into vertical space */
         points[i].y = box_top + 28 - (state->wpm[i] - min) * (box_h - 10) / range;
     }
