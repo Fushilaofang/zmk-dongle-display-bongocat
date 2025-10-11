5f17bd2f4a452a4ea5638793f4e0f88dca75c4af
fushilaofang <1770438260@qq.com>
2025-10-12 04:32:33 +0800

    撤销wpm刷新频率修改

--- boards/shields/dongle_display/widgets/status.c
+++ boards/shields/dongle_display/widgets/status.c
@@ -33,15 +33,6 @@ struct wpm_status_state {
     uint8_t wpm;
 };
 
-/* Graph layout within the 68x42 canvas */
-#define GRAPH_LEFT 2
-#define GRAPH_RIGHT (CANVAS_SIZE_W - 3)
-#define GRAPH_TOP 2
-#define GRAPH_BOTTOM (CANVAS_SIZE_H - 14)
-#define GRAPH_WIDTH (GRAPH_RIGHT - GRAPH_LEFT)
-#define GRAPH_HEIGHT (GRAPH_BOTTOM - GRAPH_TOP)
-#define TEXT_AREA_TOP (GRAPH_BOTTOM + 2)
-
 static void draw_wpm_background(struct zmk_widget_status *widget) {
     lv_obj_t *canvas = widget->obj;
 
@@ -53,33 +44,27 @@ static void draw_wpm_background(struct zmk_widget_status *widget) {
     init_rect_dsc(&rect_border_dsc, LVGL_FOREGROUND);
 
     lv_canvas_fill_bg(canvas, LVGL_BACKGROUND, LV_OPA_COVER);
-    lv_canvas_draw_rect(canvas, GRAPH_LEFT - 1, GRAPH_TOP - 1, GRAPH_WIDTH + 2, GRAPH_HEIGHT + 2,
-                        &rect_border_dsc);
-    lv_canvas_draw_rect(canvas, GRAPH_LEFT, GRAPH_TOP, GRAPH_WIDTH, GRAPH_HEIGHT, &rect_bg_dsc);
+    lv_canvas_draw_rect(canvas, 0, 21, 68, 42, &rect_border_dsc);
+    lv_canvas_draw_rect(canvas, 1, 22, 66, 40, &rect_bg_dsc);
 
     lv_canvas_set_buffer(canvas, widget->cbuf, CANVAS_SIZE_W, CANVAS_SIZE_H, LV_IMG_CF_TRUE_COLOR);
-    memcpy(widget->cbuf, widget->bg_cbuf, sizeof(widget->bg_cbuf));
+    memcpy(widget->cbuf, widget->bg_cbuf, sizeof(widget->cbuf));
 }
 
 static void draw_wpm_canvas(struct zmk_widget_status *widget) {
     lv_obj_t *canvas = widget->obj;
     const struct status_state *state = &widget->state;
 
-    memcpy(widget->cbuf, widget->bg_cbuf, sizeof(widget->bg_cbuf));
+    memcpy(widget->cbuf, widget->bg_cbuf, sizeof(widget->cbuf));
 
     lv_draw_label_dsc_t label_dsc_wpm;
     init_label_dsc(&label_dsc_wpm, LVGL_FOREGROUND, &lv_font_unscii_8, LV_TEXT_ALIGN_RIGHT);
     lv_draw_line_dsc_t line_dsc;
     init_line_dsc(&line_dsc, LVGL_FOREGROUND, 1);
-    line_dsc.round_start = 1;
-    line_dsc.round_end = 1;
-    line_dsc.rounded = 1;
 
     char wpm_text[6] = {};
     snprintf(wpm_text, sizeof(wpm_text), "%d", state->wpm[9]);
-    const int text_box_width = 24;
-    const int text_x = GRAPH_RIGHT - text_box_width + 2;
-    lv_canvas_draw_text(canvas, text_x, TEXT_AREA_TOP, text_box_width, &label_dsc_wpm, wpm_text);
+    lv_canvas_draw_text(canvas, 42, 52, 24, &label_dsc_wpm, wpm_text);
 
     int max = 0;
     int min = 256;
@@ -100,9 +85,8 @@ static void draw_wpm_canvas(struct zmk_widget_status *widget) {
 
     lv_point_t points[10];
     for (int i = 0; i < 10; i++) {
-        points[i].x = GRAPH_LEFT + (GRAPH_WIDTH * i) / 9;
-        int value = (state->wpm[i] - min) * (GRAPH_HEIGHT - 2) / range;
-        points[i].y = GRAPH_BOTTOM - 1 - value;
+        points[i].x = 2 + i * 7;
+        points[i].y = 60 - (state->wpm[i] - min) * 36 / range;
     }
     lv_canvas_draw_line(canvas, points, 10, &line_dsc);
 }
