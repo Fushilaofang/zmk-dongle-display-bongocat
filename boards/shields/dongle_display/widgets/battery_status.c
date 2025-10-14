fbae20aec318f2e05c16ad9fd199d83eea37979b
fushilaofang <1770438260@qq.com>
2025-10-14 16:11:01 +0800

    fix: update battery drawing function to include opacity parameter

--- boards/shields/dongle_display/widgets/battery_status.c
+++ boards/shields/dongle_display/widgets/battery_status.c
@@ -57,8 +57,8 @@ static void draw_battery(lv_obj_t *canvas, uint8_t level, bool usb_present) {
         rect_fill_dsc.border_width = 1;
     }
 
-    lv_canvas_set_px(canvas, 0, 0, lv_color_white());
-    lv_canvas_set_px(canvas, 4, 0, lv_color_white());
+    lv_canvas_set_px(canvas, 0, 0, lv_color_white(), LV_OPA_COVER);
+    lv_canvas_set_px(canvas, 4, 0, lv_color_white(), LV_OPA_COVER);
 
     if (level <= 10 || usb_present) {
         canvas_draw_rect(canvas, 1, 2, 3, 5, &rect_fill_dsc);
