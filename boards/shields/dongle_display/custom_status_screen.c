be9bcf0e310e7cf1b800962e77c49e522caa476b
fushilaofang <1770438260@qq.com>
2025-10-14 17:42:45 +0800

    fix: 设置屏幕背景颜色为黑色

--- boards/shields/dongle_display/custom_status_screen.c
+++ boards/shields/dongle_display/custom_status_screen.c
@@ -43,6 +43,9 @@ lv_obj_t *zmk_display_status_screen() {
 
     screen = lv_obj_create(NULL);
 
+    // 设置屏幕背景颜色为黑色
+    lv_obj_set_style_bg_color(screen, lv_color_black(), LV_PART_MAIN);
+
     lv_style_init(&global_style);
     lv_style_set_text_font(&global_style, &lv_font_unscii_8);
     lv_style_set_text_letter_space(&global_style, 1);
